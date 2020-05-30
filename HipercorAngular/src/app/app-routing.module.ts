8598cf233a7ea9bb6e44571b4b5a96bebe2ab5f7
LeandroVll <65376809+LeandroVll@users.noreply.github.com>
2020-05-30 12:28:45 -0700

    Add files via upload

--- HipercorAngular/src/app/app-routing.module.ts
+++ HipercorAngular/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 // --- Referencias de componentes a importar en funcion de la ruta --- 
 import { RegistroComponent } from './componentes/zonaCliente/registro.component';
 import { LoginComponent } from "./componentes/zonaCliente/login.component";
-import { TiendaComponent } from './componentes/zonaTienda/tienda.component';
-
+import { PanelUsuarioComponent } from "./componentes/zonaCliente/panel-usuario.component";
+import { TiendaComponent } from "./componentes/zonaTienda/tienda.component";
+import { PedidoComponent } from './componentes/zonaTienda/pedido.component';
+import { DetalleProductoComponent } from './componentes/zonaTienda/detalle-producto.component';
 
 const routes: Routes = [
 
@@ -16,10 +18,25 @@ const routes: Routes = [
                                   path: "login", component: LoginComponent //<--Ruta de acceso http://localhost:4200/cliente/loginComponent
                                 },
                                 {
-                                  path: "tienda", component: TiendaComponent //<--Ruta de acceso http://localhost:4200/cliente/tienda
-                                }
-                            ]
+                                  path: "PanelUsuario", component: PanelUsuarioComponent //<--Ruta de acceso http://localhost:4200/cliente/loginComponent
+                                }/*,
+                                {
+                                  path: "tienda", component: TiendaComponent //<--Ruta de acceso http://localhost:4200/cliente/loginComponent
+                                }*/
+                            ],
+                            
+
   },
+
+  {path: "Tienda", children:[
+
+                            {
+                              path: "productos", component: TiendaComponent //
+                            },
+                            {
+                              path: "DetalleProducto", component: DetalleProductoComponent //
+                            }
+  ]}
 
 ];
 
